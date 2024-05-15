7c3fc8f74ab559c1cac1186057ea2633a8919af1
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2024-05-15 10:40:29 +0300

    vita

--- src/mg/itu/prom16/FrontController.java
+++ src/mg/itu/prom16/FrontController.java
@@ -1,23 +1,39 @@
 package mg.itu.prom16;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
+import java.util.ArrayList;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import mg.itu.prom16.Annotations.Controller;
 
-public class FrontController extends HttpServlet{
-    Ar
-    
+public class FrontController extends HttpServlet {
+    protected ArrayList<String> controllerArrayList = new ArrayList<String>();
+
+    public void getControllerList(String packagename) throws Exception {
+        String bin_path = "WEB-INF/classes/" + packagename.replace(".", "/");
+
+        bin_path = getServletContext().getRealPath(bin_path);
+
+        File b = new File(bin_path);
+        for (File onefile : b.listFiles()) {
+            if (onefile.isFile() && onefile.getName().endsWith(".class")) {
+                Class<?> clazz = Class.forName(packagename + "." + onefile.getName().split(".class")[0]);
+                if (clazz.isAnnotationPresent(mg.itu.prom16.Annotations.Controller.class))
+                    controllerArrayList.add(clazz.getName());
+            }
+        }
+    }
+
     @Override
     public void init() throws ServletException {
-        // TODO Auto-generated method stub
         super.init();
     }
-    
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         processRequest(req, resp);
@@ -28,8 +44,20 @@ public class FrontController extends HttpServlet{
         processRequest(req, resp);
     }
 
-    protected void processRequest(HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException {
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
-        out.println(req.getRequestURI());
+        try {
+            getControllerList(getServletContext().getInitParameter("controllerPackage"));
+
+            for (String contr : controllerArrayList) {
+                out.println(contr);
+            }
+        } catch (Exception e) {
+            out.println(e.getMessage());
+            for (StackTraceElement ste : e.getStackTrace()) {
+                out.println(ste);
+            }
+        }
     }
 }
