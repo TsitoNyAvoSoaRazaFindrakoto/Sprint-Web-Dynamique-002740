78a4ce30c5a8b0b921f73113af3dccdfb486f49d
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2024-05-28 11:46:35 +0300

    first

--- src/mg/itu/prom16/utils/AnnotationFinder.java
+++ src/mg/itu/prom16/utils/AnnotationFinder.java
@@ -1,13 +1,49 @@
 package mg.itu.prom16.utils;
 
+import java.io.File;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import jakarta.servlet.ServletContext;
 import mg.itu.prom16.Annotations.Get;
 
 public class AnnotationFinder {
-    public static HashMap<String, Mapping> allGetMethods(Class<?> location){
+    
+    private static ArrayList<Class<?>> getControllerList(ServletContext sc ,String packagename) {
+        String bin_path = "WEB-INF/classes/" + packagename.replace(".", "/");
+
+        bin_path = sc.getRealPath(bin_path);
+        
+        File b = new File(bin_path);
+
+        ArrayList<Class<?>> controllerArrayList = new ArrayList<Class<?>>();
+
+        for (File onefile : b.listFiles()) {
+            if (onefile.isFile() && onefile.getName().endsWith(".class")) {
+                Class<?> clazz;
+                try {
+                    clazz = Class.forName(packagename + "." + onefile.getName().split(".class")[0]);
+                    if (clazz.isAnnotationPresent(mg.itu.prom16.Annotations.Controller.class))
+                        controllerArrayList.add(clazz);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return controllerArrayList;
+    }
+
+    public static  HashMap<String,Mapping> urlMapping(ServletContext sc ,String initparameter){
+        ArrayList<Class<?>> clazzList = AnnotationFinder.getControllerList(sc,sc.getInitParameter(initparameter));
+        HashMap<String,Mapping> urlMap = new HashMap<String,Mapping>();
+        for(Class<?> clazz : clazzList){
+            urlMap.putAll(AnnotationFinder.allGetMethods(clazz));
+        }
+        return urlMap;
+    }
+    private static HashMap<String, Mapping> allGetMethods(Class<?> location){
         HashMap<String,Mapping> map = new HashMap<String,Mapping>();
         Method[] allMethods = location.getDeclaredMethods();
 
@@ -18,8 +54,7 @@ public class AnnotationFinder {
             }
         }
 
-        return map;
-        
+        return map;  
     }
 
 }
