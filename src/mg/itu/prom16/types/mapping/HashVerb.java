a6f4f227bdce41b8f76ef5b44111a54c9abb3ac6
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2024-10-14 08:02:01 +0300

    Squashed commit of the following:
    commit 1f5df46c2f0db0295bd6ce5dfc721341e9d1a3a7
    Author: Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
    Date:   Thu Oct 10 09:25:17 2024 +0300
    
        finish no test yet
    
    commit 653b85b2e0cd9f4bed72726ea4addadbba7454ff
    Author: Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
    Date:   Wed Oct 9 20:04:54 2024 +0300
    
        sprint 10

---
+++ src/mg/itu/prom16/types/mapping/HashVerb.java
@@ -0,0 +1,34 @@
+package mg.itu.prom16.types.mapping;
+
+import java.util.HashMap;
+import java.lang.reflect.Method;
+
+public class HashVerb extends HashMap<String, Method> {
+	@Override
+	public Method put(String key, Method value) throws IllegalArgumentException {
+		expectError(key, value);
+		return super.put(key, value);
+	}
+
+	public boolean isValableKey(String key) {
+		switch (key) {
+			case "GET":
+			case "POST":
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	public Class<?> getDeclaringClass(String key){
+		return get(key).getDeclaringClass();
+	}
+
+	public void expectError(String key, Method value) throws IllegalArgumentException {
+		if (!isValableKey(key)) {
+			throw new IllegalArgumentException("Verb '" + key + "'' not allowed . only 'GET','POST' for now");
+		} else if (containsKey(key)) {
+			throw new IllegalArgumentException("Verb '" + key + " already exists");
+		}
+	}
+}
