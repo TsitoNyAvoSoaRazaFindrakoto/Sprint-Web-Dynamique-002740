6e4679aef1b5b3fefae7122461e6fc01c3d2c0c2
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-02-18 06:12:38 +0300

    ok

--- src/mg/itu/prom16/Annotations/request/Controller.java
+++
@@ -1,9 +0,0 @@
-package mg.itu.prom16.annotations.request;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface Controller {
-  boolean isController() default true;
-}
--- src/mg/itu/prom16/annotations/validation/Validator.java
+++
@@ -1,7 +0,0 @@
-package mg.itu.prom16.annotations.validation;
-
-import java.lang.reflect.Field;
-
-public interface Validator {
-	public String isvalid(Field f, Object arg) throws Exception;
-}
